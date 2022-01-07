c9073dd9ea6ae95f8152bb622aeec64d8f774ffa
abdelaziz321 <abdelazizmahmoud321@gmail.com>
2022-01-07 14:25:29 +0200

    adding categories list component

--- src/app/modules/products/products-page/components/categories-list/categories-list.component.ts
+++ src/app/modules/products/products-page/components/categories-list/categories-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Category } from '../../models/category';
+
 
 @Component({
   selector: 'app-categories-list',
@@ -6,6 +8,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./categories-list.component.scss']
 })
 export class CategoriesListComponent implements OnInit {
+  categories: Category[] = [
+    { id: 1, name: 'Plants' },
+    { id: 2, name: 'Flowers'  },
+    { id: 3, name: 'Fishes' },
+  ];
 
   constructor() { }
 
