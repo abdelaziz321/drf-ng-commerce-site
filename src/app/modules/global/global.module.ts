3de5e48440fe78f1a4f1aabf4b93c89a384a7a49
abdelaziz321 <abdelazizmahmoud321@gmail.com>
2022-01-08 22:07:17 +0200

    adding not-found page

--- src/app/modules/global/global.module.ts
+++ src/app/modules/global/global.module.ts
@@ -5,6 +5,7 @@ import { PageComponent } from './components/page/page.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { UnauthorizedComponent } from './components/unauthorized/unauthorized.component';
 import { ContactUsComponent } from './components/contact-us/contact-us.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
@@ -16,6 +17,7 @@ import { ContactUsComponent } from './components/contact-us/contact-us.component
   ],
   imports: [
     CommonModule,
+    SharedModule
   ]
 })
 export class GlobalModule { }
